8453eec5c788ac1a5c6affe3e27443c46023a66d
izarra-ch <eizarrac@uni.pe>
2024-11-17 15:56:38 -0500

    feat: Generación de proyecto base

---
+++ src/test/java/com/services/sicol/app/SicolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.sicol.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
