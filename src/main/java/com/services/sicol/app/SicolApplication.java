661e8bb476388b25e4fdcf0dc4fb751e269e2d01
Elmer Izarra <92875514+izarra-ch@users.noreply.github.com>
2024-11-17 16:10:50 -0500

    feat: Generación de proyecto base (#1)

---
+++ src/main/java/com/services/sicol/app/SicolApplication.java
@@ -0,0 +1,13 @@
+package com.services.sicol.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicolApplication.class, args);
+	}
+
+}
